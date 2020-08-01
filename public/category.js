c6eff6699f8c577f82cf4ec345c33e7c44249c48
Devin Swanson <49795858+dswan1992@users.noreply.github.com>
2020-07-31 22:12:43 -0700

    Category now includes categories.js
    Added Jordan's graphics into the get request, loads chart data when loading data from Get request

--- public/category.js
+++ public/category.js
@@ -1,5 +1,5 @@
 var categoriesDisplayed = [];
-
+var categoryArray = []
 function addToTable(item){
     let myTable = document.getElementById("category_table");
     let row = document.createElement("tr");
@@ -18,7 +18,6 @@ function updateCategories(item){
     let value = row.appendChild(cell);
     row.appendChild(cell2);
     value.innerText = `${item.category_name}`;
-    console.log(item.tasks_completed)
     for(i = 0; i < item.tasks_completed; i++){
         let check = document.createElement('i');
         check.className ="fa fa-2x fa-check-circle";
@@ -36,6 +35,103 @@ window.addEventListener('load', function(event){
             categoriesDisplayed = JSON.parse(req.responseText);
             categoriesDisplayed.forEach(addToTable);
             categoriesDisplayed.forEach(updateCategories);
+            /****************************************************************************************
+             * Original Author: Jordan Pemberton
+             * Original File: /public/categories.js
+            ****************************************************************************************/
+            // Chart Elements:
+            const barChart = document.querySelector('#chart1').getContext('2d');
+            const pieChart = document.querySelector('#chart2').getContext('2d');
+
+            const colorPalette = [  '#d62d20', '#ff5400', '#80b918', '#008744', '#02c39a', '#2a9d8f', '#5f0f40', 
+                        '#e63946', '#390099', '#ffa700', '#ff0054', '#25badf', '#ef6412', '#55006a', '#0057e7' ];
+
+            // Get categories and Tasks completed
+            let categories = [];
+            for(i = 0; i < categoriesDisplayed.length; i++){
+                categories[i] = categoriesDisplayed[i].category_name;
+            }
+            let categoryTasksCompleted = [];
+            for(i = 0; i < categoriesDisplayed.length; i++){
+                categoryTasksCompleted[i] = categoriesDisplayed[i].tasks_completed;
+            }
+            console.log(categories);
+            // Pick Colors:
+            let {transpColors, solidColors} = assignColors(categoryTasksCompleted.length);
+
+            // Make Color Palletes:
+            function assignColors(dataLen) {
+                let transpColors = [];
+                let solidColors = [];
+                let pLen = colorPalette.length;
+                for (i=0; i<dataLen; i++) {
+                    let transpColor = colorPalette[i%pLen] + '74';
+                    let solidColor = colorPalette[i%pLen];
+                    transpColors.push(transpColor); 
+                    solidColors.push(solidColor);
+                }
+                return {transpColors, solidColors};
+            }   
+
+
+            function makeBarChart(canvas) {
+                new Chart(canvas, {
+                    type: 'bar',
+                    data: {
+                    labels: categories,
+                    datasets: [{
+                        data: categoryTasksCompleted,
+                        backgroundColor: transpColors,
+                        borderColor: solidColors,
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Tasks Completed'
+                            },
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+                });
+            }
+
+            function makeDoughnutChart(canvas) {
+                new Chart(canvas, {
+                    type: 'doughnut',
+                    data: {
+                        labels: categories,
+                        datasets: [{
+                            data: categoryTasksCompleted,
+                            backgroundColor: solidColors
+                        }]
+                    },
+                    options: { 
+                        cutoutPercentage: 35,
+                        legend: {
+                            display: true,
+                            position: 'right',
+                            align: 'right',
+                            labels: {
+                            boxWidth: 12
+                            }
+                        }
+                    }
+                });
+            }   
+
+
+        makeBarChart(barChart);
+        makeDoughnutChart(pieChart);
         }
         else{
         }
