bba930489ffa944bc9811b14ee45048f126c3185
Devin Swanson <49795858+dswan1992@users.noreply.github.com>
2020-08-08 15:26:45 -0700

    Fixed Async issues

--- public/category.js
+++ public/category.js
@@ -25,18 +25,20 @@ function updateCategories(item){
 }
 
 var tasksInCategory = [];
-window.addEventListener('load', function(event){
-    var req = new XMLHttpRequest();
-    req.open('GET', 'http://localhost:8000/api/tasks', false);
-    req.setRequestHeader("Authorization", "Bearer " + window.sessionStorage.getItem('token'));
-    req.addEventListener('load', function(){
-       if(req.status >= 200 && req.status < 400){
-            tasksInCategory = JSON.parse(req.responseText); 
-        } 
-    }) 
-    req.send(null);
-    event.preventDefault;
-})
+function fillTasksInCategory(){
+    window.addEventListener('load', function(event){
+        var req = new XMLHttpRequest();
+        req.open('GET', 'http://localhost:8000/api/tasks', true);
+        req.setRequestHeader("Authorization", "Bearer " + window.sessionStorage.getItem('token'));
+        req.addEventListener('load', function(){
+           if(req.status >= 200 && req.status < 400){
+                tasksInCategory = JSON.parse(req.responseText); 
+            } 
+        }) 
+        req.send(null);
+        event.preventDefault;
+    })
+}
 
 function fillTimeSpentDictionary(responseObject, fillObject,){
     responseObject.forEach(category=> {
@@ -53,7 +55,7 @@ function fillTimeSpentDictionary(responseObject, fillObject,){
 function fillTasksCompleteDictionary(responseObject, fillObject){
     responseObject.forEach(category=> {
         if(fillObject[category.category_name]){
-            fillObject[category.category_name] += category.tasks_completed;
+            fillObject[category.category_name] = category.tasks_completed;
         }
         else{
             fillObject[category.category_name] = category.tasks_completed;
@@ -61,140 +63,141 @@ function fillTasksCompleteDictionary(responseObject, fillObject){
     })
     return fillObject;
 }
-
-window.addEventListener('load', function(event){
-    var categoriesDisplayed = [];
-    var req = new XMLHttpRequest();
-    req.open('GET', 'http://localhost:8000/api/categories', false);
-    req.setRequestHeader("Authorization", "Bearer " + window.sessionStorage.getItem('token'));
-    req.addEventListener('load', function(){
-        if(req.status >= 200 && req.status < 400){
-            categoriesDisplayed = JSON.parse(req.responseText);
-            categoriesDisplayed.forEach(addToTable);
-            categoriesDisplayed.forEach(updateCategories);
-
-            let tasks = {}
-            tasks = fillTimeSpentDictionary(tasksInCategory, tasks);
+function populateCharts(){
+    window.addEventListener('load', function(event){
+        var categoriesDisplayed = [];
+        var req = new XMLHttpRequest();
+        req.open('GET', 'http://localhost:8000/api/categories', true);
+        req.setRequestHeader("Authorization", "Bearer " + window.sessionStorage.getItem('token'));
+        req.addEventListener('load', function(){
+            if(req.status >= 200 && req.status < 400){
+                categoriesDisplayed = JSON.parse(req.responseText);
+                categoriesDisplayed.forEach(addToTable);
+                categoriesDisplayed.forEach(updateCategories);
+                console.log(tasksInCategory)
+                let tasks = {}
+                tasks = fillTimeSpentDictionary(tasksInCategory, tasks);
             
 
-            let categories = {};
-            categories = fillTasksCompleteDictionary(tasksInCategory, categories);
+                let categories = {};
+                categories = fillTasksCompleteDictionary(tasksInCategory, categories);
             
 
-            let barChartArray = Object.values(tasks);
-            barChartArray.forEach(function(item, index){
-                barChartArray[index] = (item / 60);
-            })
-            let categoryNames = Object.keys(tasks);
-            let numberOfTasksCompleted = Object.values(categories);
+                let barChartArray = Object.values(tasks);
+                barChartArray.forEach(function(item, index){
+                    barChartArray[index] = (item / 60);
+                })
+                let categoryNames = Object.keys(tasks);
+                let numberOfTasksCompleted = Object.values(categories);
 
             
-            /****************************************************************************************
-             * Original Author: Jordan Pemberton
-             * Original File: /public/categories.js
-            ****************************************************************************************/
-            // Chart Elements:
-            const barChart = document.querySelector('#chart1').getContext('2d');
-            const pieChart = document.querySelector('#chart2').getContext('2d');
+                /****************************************************************************************
+                 * Original Author: Jordan Pemberton
+                 * Original File: /public/categories.js
+                ****************************************************************************************/
+                // Chart Elements:
+                const barChart = document.querySelector('#chart1').getContext('2d');
+                const pieChart = document.querySelector('#chart2').getContext('2d');
 
-            const colorPalette = [  '#d62d20', '#ff5400', '#80b918', '#008744', '#02c39a', '#2a9d8f', '#5f0f40', 
-                        '#e63946', '#390099', '#ffa700', '#ff0054', '#25badf', '#ef6412', '#55006a', '#0057e7' ];
+                const colorPalette = [  '#d62d20', '#ff5400', '#80b918', '#008744', '#02c39a', '#2a9d8f', '#5f0f40', 
+                            '#e63946', '#390099', '#ffa700', '#ff0054', '#25badf', '#ef6412', '#55006a', '#0057e7' ];
 
             
-            // Pick Colors:
-            let {transpColors, solidColors} = assignColors(Object.keys(categories).length);
+                // Pick Colors:
+                let {transpColors, solidColors} = assignColors(Object.keys(categories).length);
 
-            // Make Color Palletes:
-            function assignColors(dataLen) {
-                let transpColors = [];
-                let solidColors = [];
-                let pLen = colorPalette.length;
-                for (i=0; i<dataLen; i++) {
-                    let transpColor = colorPalette[i%pLen] + '74';
-                    let solidColor = colorPalette[i%pLen];
-                    transpColors.push(transpColor); 
-                    solidColors.push(solidColor);
-                }
-                return {transpColors, solidColors};
-            }   
-
-
-            function makeBarChart(canvas) {
-                new Chart(canvas, {
-                    type: 'bar',
-                    data: {
-                    labels: categoryNames,
-                    datasets: [{
-                        data: barChartArray,
-                        backgroundColor: transpColors,
-                        borderColor: solidColors,
-                        borderWidth: 2
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            scaleLabel: {
-                                display: true,
-                                labelString: 'Time Spent (in min)'
-                            },
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }],
-                        xAxes: [{
-                            scaleLabel: {
-                                display: true,
-                                labelString: 'Category Name'
-                            }
-                        }]
-                    },
-                    legend: {
-                        display: false
+                // Make Color Palletes:
+                function assignColors(dataLen) {
+                    let transpColors = [];
+                    let solidColors = [];
+                    let pLen = colorPalette.length;
+                    for (i=0; i<dataLen; i++) {
+                        let transpColor = colorPalette[i%pLen] + '74';
+                        let solidColor = colorPalette[i%pLen];
+                        transpColors.push(transpColor); 
+                        solidColors.push(solidColor);
                     }
-                }
-                });
-            }
+                    return {transpColors, solidColors};
+                }   
 
-            function makeDoughnutChart(canvas) {
-                new Chart(canvas, {
-                    type: 'doughnut',
-                    data: {
+
+                function makeBarChart(canvas) {
+                    new Chart(canvas, {
+                        type: 'bar',
+                        data: {
                         labels: categoryNames,
                         datasets: [{
-                            data: numberOfTasksCompleted,
-                            backgroundColor: solidColors
+                            data: barChartArray,
+                            backgroundColor: transpColors,
+                            borderColor: solidColors,
+                            borderWidth: 2
                         }]
                     },
-                    options: { 
-                        cutoutPercentage: 35,
-                        legend: {
-                            display: true,
-                            position: 'right',
-                            align: 'right',
-                            labels: {
-                            boxWidth: 12
-                            }
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Time Spent (in min)'
+                                },
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }],
+                            xAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Category Name'
+                                }
+                            }]
                         },
-                        title: {
-                            position: 'top',
-                            display: true,
-                            text: 'Number of Tasks Completed Per Category'
+                        legend: {
+                            display: false
                         }
                     }
-                });
-            }   
+                    });
+                }
+
+                function makeDoughnutChart(canvas) {
+                    new Chart(canvas, {
+                        type: 'doughnut',
+                        data: {
+                            labels: categoryNames,
+                            datasets: [{
+                                data: numberOfTasksCompleted,
+                                backgroundColor: solidColors
+                            }]
+                        },
+                        options: { 
+                            cutoutPercentage: 35,
+                            legend: {
+                                display: true,
+                                position: 'right',
+                                align: 'right',
+                                labels: {
+                                boxWidth: 12
+                                }
+                            },
+                            title: {
+                                position: 'top',
+                                display: true,
+                                text: 'Number of Tasks Completed Per Category'
+                            }
+                        }
+                    });
+                }   
 
 
-        makeBarChart(barChart);
-        makeDoughnutChart(pieChart);
-        }
-        else{
-        }
-    });
-    req.send(null);
-    event.preventDefault;
-})
+            makeBarChart(barChart);
+            makeDoughnutChart(pieChart);
+            }
+            else{
+            }
+        });
+        req.send(null);
+        event.preventDefault;
+    })
+}
 /****************************************************
  * End of Jordan's Code
  ***************************************************/
@@ -230,5 +233,6 @@ const getCategoryData = () => {
 }
 
 
-
+document.addEventListener("load", fillTasksInCategory());
+document.addEventListener("load", populateCharts());
 document.addEventListener("load", login());
